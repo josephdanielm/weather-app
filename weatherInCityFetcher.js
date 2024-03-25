f2cd7e9df679b51a793e40cf7efb5a7626036c4f
Joseph Mudd <josephmuddjr@gmail.com>
2024-03-25 05:34:52 -0500

    fix?

--- weatherInCityFetcher.js
+++ weatherInCityFetcher.js
@@ -3,7 +3,7 @@ import processWeatherData from "./weatherDataProcessor";
 export default async function getWeatherIn(city) {
     try {
 
-        const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=b43a9d1e821b4ba5b2b04526241803&q=${city}&days=3&aqi=no&alerts=no`, {
+        const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=b43a9d1e821b4ba5b2b04526241803&q=${city}&days=3&aqi=no&alerts=no`, {
             mode: 'cors'
         });
 
